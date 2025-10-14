5f142ea7f078f8cb44468a51e879ab5dd9a8607d
akaringo-15 <funa12272003@gmail.com>
2025-10-14 23:13:47 +0900

    add: つなげるノーツの判定範囲拡大できたと思っている

--- sandbox/connect_note_sample/Assets/ConnectNotes_Position.cs
+++ sandbox/connect_note_sample/Assets/ConnectNotes_Position.cs
@@ -37,7 +37,7 @@ public class ConnectNotes_Position : MonoBehaviour
         }
     }*/
 
-    public void GetMouseXOnCubeMM(GameObject target)
+    /*public void GetMouseXOnCubeMM(GameObject target)
     {
         if (Input.GetMouseButton(0) && target != null)
         {
@@ -54,5 +54,36 @@ public class ConnectNotes_Position : MonoBehaviour
             
         }       
 
+    }*/
+
+    public void GetMouseXOnCubeMM(GameObject target)
+    {
+        if (Input.GetMouseButton(0) && target != null)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Rayが何かに当たった場合
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Cubeに直接当たった場合（これまで通り）
+                if (hit.collider.gameObject == target)
+                {
+                    localPos = target.transform.InverseTransformPoint(hit.point);
+                    Debug.Log("当たってる");
+                }
+                else
+                {
+                    // Cubeに当たらなくても、中心との距離が10cm以内なら有効とする
+                    float distance = Vector3.Distance(hit.point, target.transform.position);
+                    if (distance <= 0.1f)
+                    {
+                        localPos = target.transform.InverseTransformPoint(hit.point);
+                        Debug.Log("外れた");
+                    }
+                }
+            }
+        }
     }
+
 }
