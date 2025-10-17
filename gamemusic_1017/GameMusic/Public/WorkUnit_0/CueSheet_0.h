4541490fe2b2a84250aa13ff1bab6d6ebc36dd21
akaringo-15 <funa12272003@gmail.com>
2025-10-18 00:35:43 +0900

    add: 本番のゲーム用

---
+++ gamemusic_1017/GameMusic/Public/WorkUnit_0/CueSheet_0.h
@@ -0,0 +1,26 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : GameMusic
+ *  Tool Ver.        : Ver.3.55.09
+ *  ACB  Ver.        : Ver.1.44.1
+ *  File Path        : D:/3d-rhythm-unity/新しいフォルダー/GameMusic/Public/WorkUnit_0
+ *  File Name        : CueSheet_0.h
+ *  File Size        : 1913856 bytes
+ *  Date Time        : 2025/10/17 17:41:44
+ *  Target           : Public
+ *  Cues             : 1
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : D:/3d-rhythm-unity/新しいフォルダー/GameMusic/Public/WorkUnit_0/CueSheet_0.awb
+ *===========================================================================*/
+
+#define CRI_CUESHEET_0_CUENUM (1)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_CUESHEET_0_GAMEMUSIC            ( 0) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
